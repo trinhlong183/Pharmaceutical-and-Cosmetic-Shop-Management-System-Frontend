3e3747b3e29db03d1c552177664747cd526743e8
trinhlong183 <trinhlong1803@gmail.com>
2025-06-08 23:52:10 +0700

    fix error category

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,16 +1,16 @@
-"use client";
-
 import { ReactNode } from "react";
-import AdminSidebar from "@/components/admin/AdminSidebar";
-import AdminHeader from "@/components/admin/AdminHeader";
+// import AdminSidebar from "@/components/admin/AdminSidebar";
+// import AdminHeader from "@/components/admin/AdminHeader";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex h-screen">
-      <AdminSidebar />
+      {/* <AdminSidebar /> */}
       <div className="flex flex-col flex-1 overflow-hidden">
-        <AdminHeader />
-        <main className="flex-1 overflow-y-auto p-4 bg-gray-50">{children}</main>
+        {/* <AdminHeader /> */}
+        <main className="flex-1 overflow-y-auto p-4 bg-gray-50">
+          {children}
+        </main>
       </div>
     </div>
   );
